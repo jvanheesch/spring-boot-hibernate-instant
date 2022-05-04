46d078a94f39ce8ce3b67bb5051f7f6cf56609bb
jvanheesch <joris.vanheesch@vdab.be>
2022-05-04 11:30:34 +0200

    add jpa auditing: instant is serialized in current jvm timezone (jdbc: https://stackoverflow.com/questions/508019/how-to-store-date-time-and-timestamps-in-utc-time-zone-with-jpa-and-hibernate)

--- src/main/java/com/github/jvanheesch/Application.java
+++ src/main/java/com/github/jvanheesch/Application.java
@@ -3,9 +3,11 @@ package com.github.jvanheesch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     @Autowired
--- src/main/java/com/github/jvanheesch/Author.java
+++ src/main/java/com/github/jvanheesch/Author.java
@@ -1,16 +1,24 @@
 package com.github.jvanheesch;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.Instant;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @CreatedDate
+    private Instant createdTimestamp;
 
     public Long getId() {
         return id;
@@ -26,5 +34,13 @@ public class Author {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Instant createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
     }
 }
